9215b26922c23460558232d7e0e787697d267e9b
GANSGX <takeshigans@gmail.com>
2024-11-03 09:54:34 -0800

    add-order-button-modal-window

--- js/app.js
+++ js/app.js
@@ -158,3 +158,39 @@ document.addEventListener('DOMContentLoaded', function() {
         indicator.style.right = `${100 - (100 / num) - (100 / num) * (newPos - 1)}%`;
     }
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modal");
+    const openButton = document.querySelector(".unique-order-button");
+    const closeButton = document.querySelector(".close-button");
+
+    // Функция для открытия модального окна
+    openButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Отменяем действие по умолчанию
+        modal.style.display = "block"; // Показываем модальное окно
+    });
+
+    // Функция для закрытия модального окна
+    function closeModal() {
+        modal.style.display = "none"; // Скрываем модальное окно
+    }
+
+    // Закрытие модального окна при клике на кнопку закрытия
+    closeButton.addEventListener("click", closeModal);
+
+    // Закрытие модального окна при клике вне его содержимого
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    // Закрытие модального окна при нажатии клавиши Esc
+    window.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            closeModal();
+        }
+    });
+});
